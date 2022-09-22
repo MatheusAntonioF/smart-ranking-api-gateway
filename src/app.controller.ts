949824dfae57e33d33bf224423ff55169fb9c9c8
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-09-22 19:09:36 -0300

    feat: route to get categories
    implementing method requestor/responser from nest to call request and wait to data from microservices

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
 import {
   Body,
   Controller,
-  Logger,
+  Get,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -16,27 +17,31 @@ import {
   ClientProxyFactory,
   Transport,
 } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 import { CreateCategoryDTO } from './dtos/create-category.dto';
 
 @Controller('api/v1')
 export class AppController {
-  private logger = new Logger(AppController.name);
-
   private clientAdminBackend: ClientProxy;
 
   constructor() {
     this.clientAdminBackend = ClientProxyFactory.create({
-      transport: Transport.RMQ, // param to specify the transporter to rabbitmq
+      transport: Transport.RMQ, // param to specify the transporter to rabbit mq
       options: {
-        urls: ['amqp://user:hQ8Z5bUw38g4@52.91.198.1:5672/smartranking'], // param to specify the url of the rabbitmq
+        urls: ['amqp://user:hQ8Z5bUw38g4@107.22.1.222:5672/smartranking'], // param to specify the url of the rabbit mq
         queue: 'admin-backend', // param to specify the queue name
       },
     });
   }
 
+  @Get('categories')
+  index(@Query('category_id') category_id: string): Observable<any> {
+    return this.clientAdminBackend.send('get-categories', category_id || ''); // send return a code observable
+  }
+
   @Post('categories')
   @UsePipes(ValidationPipe)
-  async createCategory(@Body() createCategory: CreateCategoryDTO) {
-    return this.clientAdminBackend.emit('create-category', createCategory);
+  create(@Body() createCategory: CreateCategoryDTO) {
+    this.clientAdminBackend.emit('create-category', createCategory); // emit return hotable observable
   }
 }
