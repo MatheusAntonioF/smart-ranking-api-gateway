a4812d40da238b8d1895c0fa0430048683bac6d4
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-09-22 19:38:13 -0300

    feat: implementing interceptors to prevent timout from micro services

---
+++ src/interceptors/timeout-interceptor.ts
@@ -0,0 +1,19 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, timeout } from 'rxjs';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const TEN_SECONDS_IN_MILLISECONDS = 10000;
+
+    return next.handle().pipe(timeout(TEN_SECONDS_IN_MILLISECONDS));
+  }
+}
